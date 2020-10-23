8bdf2dc8d57a301c744bce7be6608ebf7e29aabd
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-10-24 02:30:52 +0300

    added menu navigation

--- animations.js
+++ animations.js
@@ -10,6 +10,13 @@ const rightLine = document.getElementById("rightLine");
 const svgImg = document.getElementById("svgImg");
 const headerDiv = document.getElementById("headerDiv");
 const nameDiv = document.getElementById("nameDiv");
+const menuNavigation = document.getElementById("menuNavigation");
+const menuDiv = document.getElementById("menuDiv");
+const designMenu = document.getElementById("designMenu");
+const synchronizationMenu = document.getElementById("synchronizationMenu");
+const assistanceMenu = document.getElementById("assistanceMenu");
+const efficiencyMenu = document.getElementById("efficiencyMenu");
+const techInfoMenu = document.getElementById("techInfoMenu");
 
 comfort.onclick = function () {
     comfortDesc.style.opacity = "1";
@@ -41,9 +48,10 @@ limitedTSI.onclick = function () {
     limitedTDIDesc.style.opacity = "0";
 }
 
-window.onscroll = function () {
-    if (document.documentElement.scrollTop > document.getElementById("reviewTable").offsetTop
-        - headerDiv.offsetHeight) {
+menuDiv.onclick = function () {
+    if (menuDiv.classList.length === 0) {
+        menuNavigation.style.display = "block";
+        menuDiv.classList.add("clicked");
         leftLine.style.backgroundColor = "#001e50";
         svgImg.style.fill = "#001e50";
         rightLine.style.backgroundColor = "#001e50";
@@ -54,6 +62,8 @@ window.onscroll = function () {
         }
         nameDiv.style.color = "#001e50";
     } else {
+        menuNavigation.style.display = "none";
+        menuDiv.classList.remove("clicked");
         leftLine.style.backgroundColor = "#fff";
         svgImg.style.fill = "#fff";
         rightLine.style.backgroundColor = "#fff";
@@ -64,4 +74,71 @@ window.onscroll = function () {
         }
         nameDiv.style.color = "#fff";
     }
+}
+
+window.onscroll = function () {
+    if (menuDiv.classList.length === 0) {
+        if (document.documentElement.scrollTop > document.getElementById("reviewTable").offsetTop
+            - headerDiv.offsetHeight) {
+            leftLine.style.backgroundColor = "#001e50";
+            svgImg.style.fill = "#001e50";
+            rightLine.style.backgroundColor = "#001e50";
+            headerDiv.style.backgroundColor = "#fff";
+            let selected = headerDiv.querySelectorAll(".menu");
+            for (let elem of selected) {
+                elem.style.backgroundColor = "#001e50";
+            }
+            nameDiv.style.color = "#001e50";
+        } else {
+            leftLine.style.backgroundColor = "#fff";
+            svgImg.style.fill = "#fff";
+            rightLine.style.backgroundColor = "#fff";
+            headerDiv.style.backgroundColor = "";
+            let selected = headerDiv.querySelectorAll(".menu");
+            for (let elem of selected) {
+                elem.style.backgroundColor = "#fff";
+            }
+            nameDiv.style.color = "#fff";
+        }
+    }
+}
+
+function routine() {
+    document.documentElement.scrollTop -= headerDiv.offsetHeight;
+    menuNavigation.style.display = "none";
+    menuDiv.classList.remove("clicked");
+    leftLine.style.backgroundColor = "#fff";
+    svgImg.style.fill = "#fff";
+    rightLine.style.backgroundColor = "#fff";
+    headerDiv.style.backgroundColor = "";
+    let selected = headerDiv.querySelectorAll(".menu");
+    for (let elem of selected) {
+        elem.style.backgroundColor = "#fff";
+    }
+    nameDiv.style.color = "#fff";
+}
+
+designMenu.onclick = function () {
+    document.getElementById("design").scrollIntoView({behavior: "smooth", block: "start"});
+    routine();
+}
+
+synchronizationMenu.onclick = function () {
+    document.getElementById("synchronization").scrollIntoView({behavior: "smooth", block: "start"});
+    routine();
+}
+
+assistanceMenu.onclick = function () {
+    document.getElementById("assistance").scrollIntoView({behavior: "smooth", block: "start"});
+    routine();
+}
+
+efficiencyMenu.onclick = function () {
+    document.getElementById("efficiency").scrollIntoView({behavior: "smooth", block: "start"});
+    routine();
+}
+
+techInfoMenu.onclick = function () {
+    document.getElementById("techInfo").scrollIntoView({behavior: "smooth", block: "start"});
+    routine();
 }
